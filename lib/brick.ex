8fe8ed30a46a17f20733f65a20327922fd8fc67e
ryanyogan <ryanyogan@gmail.com>
2020-02-03 10:48:55 -0800

    movement and tests

--- lib/brick.ex
+++ lib/brick.ex
@@ -7,9 +7,9 @@ defmodule Tetris.Brick do
   @spec new :: Tetris.Brick.t()
   def new(), do: Tetris.Brick.__struct__()
 
-  @spec new_random :: %{location: {40, 0}, name: any, reflection: false, rotation: 0}
+  @spec new_random :: Tetris.Brick.t()
   def new_random() do
-    %{
+    %__MODULE__{
       name: random_name(),
       location: {40, 0},
       rotation: random_rotation(),
@@ -34,4 +34,36 @@ defmodule Tetris.Brick do
     [0, 90, 180, 270]
     |> Enum.random()
   end
+
+  def down(brick) do
+    %{brick | location: point_down(brick.location)}
+  end
+
+  def left(brick) do
+    %{brick | location: point_left(brick.location)}
+  end
+
+  def right(brick) do
+    %{brick | location: point_right(brick.location)}
+  end
+
+  def point_down({x, y}) do
+    {x, y + 1}
+  end
+
+  def point_left({x, y}) do
+    {x - 1, y}
+  end
+
+  def point_right({x, y}) do
+    {x + 1, y}
+  end
+
+  def spin_90(brick) do
+    %{brick | rotation: rotate(brick.rotation)}
+  end
+
+  def rotate(270), do: 0
+
+  def rotate(degrees), do: degrees + 90
 end
